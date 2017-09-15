67522a18985ae514ff2b488baa3c9df07d89768e
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-15 08:40:10 -0700

    Update 2_OrganiseCareerFair.java

--- 2_OrganiseCareerFair.java
+++ 2_OrganiseCareerFair.java
@@ -1,4 +1,11 @@
-/*Scenario:  2. Organise a career fair(Suppose you are the organiser)*/
+/*Scenario:  2. Organise a career fair(Suppose you are the organiser)
+2.1 Set up a questionaire website to collect student's preferred companies, career fair open dates, how many students willing to come  
+2.2 According to the questionaire result, invite companies, decide career fair open dates, reserve room that meet students'need
+2.3 Set up a career fair website for student to see career fair information detail, register check-in ticket and receive update
+2.4 
+
+
+*/
 
 //Identify Objects and Behaviors://
 

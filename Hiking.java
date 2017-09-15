889d7c8a13d42aa5ad289fcaf8a83beb86f732a8
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-14 19:02:56 -0700

    Update Hiking.java

--- Hiking.java
+++ Hiking.java
@@ -1,24 +1,40 @@
-Scenario:  1. Hiking
+/*Scenario:  1. Hiking
  
  1.1. Rent a ReachNow to drive to mountain rainier on weekends with friends
  1.2. On the way, fill the gas
  1.3. On the way, eat at restaurant for lunch
  1.4. Buy the ticket to get into the park
- 1.5. Book a hotel or check in the hotel.
+ 1.5. Book a hotel or check in the hotel.  */
  
-Identify Objects and Behaviors:
+//Identify Objects and Behaviors://
 
  Object : Hiker
- Attribute : name, email, phone, username, password
- Behaviors : loginToAccount
+ Attribute : name, emailAddr, phoneNumber, username, password
+ Behaviors : loginToAccount(), pressCarBrake(), pressCarAccelerate(), makePayment()
  
  Object : CarRentalSite
  Attribute : url, carColor, carMaker, priceRange, rentTimeRange, 
- Behaviors : searchCar, sort, display, compare , placeTheOrder 
+ Behaviors : searchCar(), sort(), compare() , placeTheOrder() 
+ 
+ Object : CarRentalSiteLoginPage
+ Attribute : welcomeMessage, accountInfo, accountSetting, 
+ Behaviors : displayUserInfo()
  
  Object : Car
- Attribute : maker, color, plateNumber
- Behaviors : fillGas
+ Attribute : carMaker, carColor, plateNumber
+ Behaviors : fillGas()
+  
+ Object : CreditCard
+ Attribute : cardNumber, expireMonth, expireYear, securityNumber
+ Behaviors : 
+  
+ Object : CreditCardCompany
+ Attribute : companyName
+ Behaviors : authorizeCard()
+  
+ Object : MapNavigator
+ Attribute : navigatorName
+ Behaviors : getRoute(), getNearbyPlaceOfInterest()
  
  Object : Gas
  Attribute : gasType, gasPrice
@@ -40,23 +56,34 @@ Identify Objects and Behaviors:
  Attribute : url, hotelList, checkInDate, checkOutDate, hotelPrice, 
  Behaviors : 
 
-Sequence of Flow - Invoke Objects with Behaviors
+
+//Sequence of Flow - Invoke Objects with Behaviors//
  
  Hiker yiteng
  CarRentalSite reachnow
+ CarRentalSiteLoginPage reachnowloginpage
  Car bmw
+ CreditCardCompany visa
+ MapNavigator googlemaps
  GasStation chevron
  Restaurant kfc
  Park rainierPark
  HotelBookingSite expedia
  
- yiteng.loginToAccount -> reachnow : authorize
- reachnow.searchCar -> priceRange, carColor, carMaker,rentTimeRange: collection of car
+ //1.1. Rent a ReachNow BMW car using VISA credit card//
+ yiteng.loginToAccount() -> username, password -> reachnow : authorize & return reachnowloginpage
+ rechnowloginpage.displayUserInfo()
+ reachnow.searchCar() -> priceRange, carColor, carMaker,rentTimeRange -> reachnow : return collection of car
  bmw = car
- yiteng.rentCar -> bmw, creditCard, address, reachnow : rentConfirmation
- response = rentConfirmation
- yiteng.searchGasStation
- car.fillGas
+ yiteng.rentCar() -> bmw, cardNumber, expireMonth, expireYear, securityNumber, address -> visa.authorizeCard() -> reachnow : return rentConfirmation
+ 
+ //1.2. On the way, use Google Maps to find route to Chevron gas station, make payment using VISA card, then fill the gas//
+ googlemaps.getNearbyPlaceOfInterest() -> gasStationName -> googlemaps.getRoute()
+ yiteng.pressCarAccelerate()
+ yiteng.pressCarBrake()
+ yiteng.makePayment() -> cardNumber, expireMonth, expireYear, securityNumber, gasType, gasPrice -> visa.authorizeCard()
+ bmw.fillGas()
+ 
  
  
 

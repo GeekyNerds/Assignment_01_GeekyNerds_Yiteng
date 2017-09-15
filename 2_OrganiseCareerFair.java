79ab05a328cef6d786d3798ef451918a3d8b0ae4
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-15 00:51:11 -0700

    Update 2_OrganiseCareerFair.java

--- 2_OrganiseCareerFair.java
+++ 2_OrganiseCareerFair.java
@@ -1,3 +1,17 @@
-/*Scenario:  2. Organise a career fair */
+/*Scenario:  2. Organise a career fair(Suppose you are the organiser)*/
 
-ddd
+//Identify Objects and Behaviors://
+
+ Object : Organisor
+ Attribute : name, emailAddr, billingAddr, phoneNumber, username, password, signature
+ Behaviors : loginToAccount()
+ 
+
+
+
+//Sequence of Flow - Invoke Objects with Behaviors//
+ 
+ Organisor yiteng
+ CarRentalSite reachnow
+ CarRentalSiteLoginPage reachnowloginpage
+

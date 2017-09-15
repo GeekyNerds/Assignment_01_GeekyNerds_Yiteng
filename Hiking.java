6de94e2475068863236503212d4b38adfe66cd43
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-14 22:02:23 -0700

    Update Hiking.java

--- Hiking.java
+++ Hiking.java
@@ -46,14 +46,18 @@
  
  Object : Restaurant
  Attribute : resName, resLocation
- Behaviors : makeMeal
+ Behaviors : makeMeal()
 
  Object : Menu
- Attribute : mealName, mealPrice
+ Attribute : mealName, mealPrice, mealAmount
  Behaviors :  
 
  Object : Park
- Attribute : ticketPrice, pointOfInterest, openTime
+ Attribute : parkName, pointOfInterest, openTime
+ Behaviors : issueTicket()
+
+ Object : Ticket
+ Attribute : ticketPrice, ticketAmount
  Behaviors : 
  
  Object : HotelBookingSite
@@ -78,28 +82,38 @@
  //1.1. Rent a ReachNow BMW car using VISA credit card//
  yiteng.loginToAccount() -> username, password -> reachnow : authorize & return LoginConfirmation object
  reachnow.searchCar() -> priceRange, carColor, carMaker,rentTimeRange -> reachnow : return Car object collection
- bmw = Car // choose BMW car (initialize Car object)
- yiteng.rentCar() -> bmw, CreditCard -> visa.authorizeCard()
- reachnow : return CarRentConfirmation object
+ yiteng.createOrder() -> Car(input: bmw) -> reachnow: return CarRentOrderConfirmation object 
+ yiteng.makePayment() -> CreditCard (input: cardNumber, expireMonth, expireYear)-> visa.authorizeCard()
+ reachnow : return CarRentPaidConfirmation object
  
  //1.2. On the way, use Google Maps to find route to Chevron gas station, make payment using VISA card, then fill the gas//
  googlemaps.getNearbyPlaceOfInterest() -> gasStationName -> googlemaps.getRoute() -> googlemaps: return Route object
  yiteng.pressCarAccelerate()
  yiteng.rotateSteerWheel()
  yiteng.pressCarBrake()
- yiteng.makePayment() -> CreditCard, gasType, gasPrice, gasAmount -> visa.authorizeCard()
- chevron: return GasPurchaseConfirmation object
+ yiteng.createOrder() -> Gas(input: gasType, gasPrice, gasAmount) -> chevron: return GasOrderConfirmation object 
+ yiteng.makePayment() -> CreditCard -> visa.authorizeCard()
+ chevron: return GasPaidConfirmation object
  chevron.outputGas()
   
- //1.3. On the way, use Google Maps to find route to KFC restaurant, make payment using VISA card, then have//
+ //1.3. On the way, use Google Maps to find route to KFC restaurant, make payment using VISA card//
  googlemaps.getNearbyPlaceOfInterest() -> resName -> googlemaps.getRoute() -> googlemaps: return Route object
  yiteng.pressCarAccelerate()
  yiteng.rotateSteerWheel()
  yiteng.pressCarBrake()
- yiteng.creatOrder() -> Menu -> kfc: return OrderConfirmation object 
+ yiteng.createOrder() -> Menu(input: mealName, mealPrice, mealAmount)-> kfc: return MealOrderConfirmation object 
  yiteng.makePayment() -> CreditCard-> visa.authorizeCard()
- kfc: return OrderPaidConfirmation object
+ kfc: return MealPaidConfirmation object
  kfc.makeMeal()
+  
+ //1.4. Buy the ticket using VISA card at Mountain Rainier Park//
+ yiteng.createOrder() -> Ticket -> rainierPark: return TicketOrderConfirmation object 
+ yiteng.makePayment() -> CreditCard-> visa.authorizeCard()
+ rainierPark: return TicketPaidConfirmation object
+ rainierPark.issueTicket()
+  
+  
+  
   
   
   

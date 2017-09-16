e9a0a95d3a7aefd0b9ee353031e7d4e9e1c590f7
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-15 19:58:24 -0700

    Update 1_Hiking.java

--- 1_Hiking.java
+++ 1_Hiking.java
@@ -1,13 +1,14 @@
 /*Scenario:  1. Hiking
  
- 1.1. Rent a ReachNow to drive to mountain rainier on weekends with friends
- 1.2. On the way, fill the gas
- 1.3. On the way, eat at restaurant for lunch
- 1.4. Buy the ticket to get into the park
- 1.5. Book a hotel or check in the hotel. 
+ 1.1. Rent a ReachNow BMW car using VISA credit card
+ 1.2. On the way, use Google Maps to find route to Chevron gas station, make payment using VISA card, then fill the gas
+ 1.3. On the way, use Google Maps to find route to KFC restaurant, make payment using VISA card
+ 1.4. Buy the ticket using VISA card at Mountain Rainier Park
+ 1.5. Book Holiday Inn via Expedia website 
  
 */
  
+
 //Identify Objects and Behaviors:
 
  Object : Hiker

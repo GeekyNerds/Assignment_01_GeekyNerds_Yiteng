ff6ecb6f99f652b1f997acbc14b9990a5e7d05b3
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-15 19:35:07 -0700

    2. Organise a career fair
    Assignment 1

--- 2_OrganiseCareerFair.java
+++ 2_OrganiseCareerFair.java
@@ -31,40 +31,62 @@
  Attribute : Question[] question
  Behaviors : 
    
- Object : Organisor
- Attribute : name, emailAddr, phoneNumber, username, password
- Behaviors : loginToAccount(), setUpWebsite()
+ Object : CareerFairWebsite
+ Attribute : url
+ Behaviors : authorize()
   
- Object : Organisor
- Attribute : name, emailAddr, phoneNumber, username, password
- Behaviors : loginToAccount(), setUpWebsite()
+ Object : LoginConfirmation
+ Attribute : welcomeMessage
+ Behaviors : 
   
- Object : Organisor
- Attribute : name, emailAddr, phoneNumber, username, password
- Behaviors : loginToAccount(), setUpWebsite()
+ Object : QuestionaireResult
+ Attribute : answeredMessage
+ Behaviors : 
+  
+ Object : InvitationResult
+ Attribute : boolean 
+ Behaviors : 
+  
+ Object : RegistrationResult
+ Attribute : registrationAmount
+ Behaviors : 
  
-
-
 
 //Sequence of Flow - Invoke Objects with Behaviors//
  
  Organisor yiteng
+ Student student
+ Company company
  QuestionaireWebsite questionwebsite
  Questionaire questionaire
  CareerFairWebsite careerwebsite
+ LoginConfirmation loginconfirmation
+ QuestionaireResult questionaireresult
+ InvitationResult invitationresult
+ RegistrationResult registrationresult
  
- 
+ //2.1 Set up a questionaire website to collect student's preferred companies, career fair open dates, how many students willing to come
  yiteng.setUpWebsite() -> Questionaire (input: question[0], question[1], ...)
  yiteng.sendEmail() -> studentEmailAddr
  student.loginAccount() -> username, password -> questionairewebsite: authorize & return LoginConfirmation object
  student.answerQuestionaire() -> companyName, preferredOpenDate, jobTitle -> questionwebsite: return QuestionaireResult object
+ 
+ //2.2 According to the questionaire result, invite companies, decide career fair open dates
  yiteng.sendEmail() -> Invitation (input: companyEmailAddr, time, location, message) -> company: return InvitationResult object
+ 
+ //2.3 According to the invitation result, reserve room that meets company & student's need
  yiteng.reserveRoom()
+  
+ //2.4 Set up a career fair website for student to see career fair information detail, register check-in ticket and receive update
  yiteng.setUpWebsite() -> openTime, fairLocation, companyName, jobTitle
  student.registerCareerFair() -> studentName, studentEmailAddr, phoneNumber -> careerwebsite: return RegistrationResult object
+ 
+ //2.5 According to the registration result, decide whether further action is needed, such as "if a larger room is needed or not?"
  if(registration amount > room capacity)
  yiteng.reserveRoom()
  end
+ 
+ //2.6 Collect students and companies feedbacks about this career fair
  student.giveFeedback()
  company.giveFeedback()
   

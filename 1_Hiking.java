62c9a095182b6fe72588e4ecc3400ea07dbb5d28
Yiteng <31704724+GeekyNerds@users.noreply.github.com>
2017-09-22 17:17:50 -0700

    Update 1_Hiking.java

--- 1_Hiking.java
+++ 1_Hiking.java
@@ -1,4 +1,4 @@
-/*Scenario:  1. Hiking
+/*----------------------------------------------------Scenario:  1. Hiking--------------------------------------------------
  
  1.1 rent a ReachNow BMW car using VISA credit card
  1.2 on the way, use Google Maps to find route to Chevron gas station, make payment using VISA card, then fill the gas
@@ -279,7 +279,7 @@
  company.giveFeedback()
   
  
-/*---------------------------------Scenario:  3. Order Pizza from Pizza Hut ---------------------------------------------
+/*----------------------------------------Scenario:  3. Order Pizza from Pizza Hut ---------------------------------------------
 
 3.1 log into PizzaHut website and get welcome messagae from PizzaHut website
 3.2 choose pizza size, crust, topping, then PizzaHut returns pizza order confirmation
@@ -450,6 +450,63 @@ public class CodeSharingPlatform{
     
 }
 
+/*---------------------------------Scenario:  5. Design a soft-drink/snacks vending machine------------------------------------------
+
+5.1 be able to validate if a user input a valid product number, if not re-enter product number
+5.2 be able to show user order price in total
+5.3 be able to validate the money that user insert, if money is not enough or invalid money inserted, ask for re-insert 
+5.4 be able to output the product that user orders
+5.5 be able to update product inventory information
+5.6 be able to alert admin to refill if certain product is in a low inventory
+
+*/
+
+//Identify Object and Behaviors:
+
+Object : VendingMachine
+Attribute : machineName, machineColor, machineMaker, location, Product[][] products
+Behaviors : validateUserOrderInput(), showOrderPrice(), validateMoneyInput(), outputProduct(), updateInventory(),
+            alertLowInventory()
+
+           
+public class VendingMachine{
+     string location;
+     string[][] product; // two-dimension array e.g. {{"001","coke"},{"002","cookie"},....}
+     string machineName
+     string machineColor
+     string machineMaker
+     
+     //validate if a user input a valid product number, if not re-enter product number
+     float validateUserOrderInput(){
+     
+     }
+     
+     //show user order price in total
+     float showOrderPrice(){
+     
+     }
+     
+     //validate the money that user insert
+     boolean validateMoneyInput(){
+     
+     }
+     
+     //output the product that user orders
+     string outputProduct(){
+     
+     }
+     
+     //update product inventory information
+     string updateInventory(){
+     
+     }
+     
+     //if certain product is in a low inventory, alert admin to refill
+     string alertLowInventory(){
+     
+     }
+
+}
    
 
   
